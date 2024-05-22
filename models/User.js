50822634935a6e315932e350c14d419d45671a60
Kabil Hussain <68674204+kabil166@users.noreply.github.com>
2024-05-23 00:05:06 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 // models/User.js
 const { DataTypes, Sequelize } = require('sequelize');
-const sequelize = new Sequelize('mysql://root:new_password@localhost:3306/linx');
+const sequelize = new Sequelize('mysql://root:new_password@13.201.36.2:3306/linx');
 
 const User = sequelize.define('User', {
   // Define model attributes
