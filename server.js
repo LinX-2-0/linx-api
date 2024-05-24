c6fc778781786792c8095790d650f59d6fa75ab0
Kabil Hussain <68674204+kabil166@users.noreply.github.com>
2024-05-24 17:15:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./api/apiRoutes.yaml');
 const PORT = 5000;
 
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'http://65.1.86.38:3000'}));
 
 
 db.connect((err) => {
